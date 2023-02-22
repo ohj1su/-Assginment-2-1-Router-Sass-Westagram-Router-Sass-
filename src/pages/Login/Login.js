482a07582b95017973bd8fd3db6695f0a7cb1a01
ohj1su <ohsj1208@gmail.com>
2023-02-22 20:33:54 +0900

    ADD: 개인별 초기세팅

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,40 +1,38 @@
-import React from "react";
-import "../Login/Login.css";
-import {useNavigate} from "react-router-dom";
-
-
-
+import React from 'react';
+import '../Login/Login.scss';
+import { useNavigate } from 'react-router-dom';
 const Login = () => {
-    const navigate =useNavigate();
+  const navigate = useNavigate();
 
-    const goToMain =() => {
-        navigate('/Main');
-    };
-    
-    return(
-<>
-   <div class="content">
-    
-        <div class="content_head">
-            <span>westagram</span>
-        </div>
- 
-        <div class="content_center">
-            <div class="center_body">
-                <input className = "center_id" type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
-                <input className = "center_pw" type="password" placeholder="비밀번호" />
-            </div>
-                <button className ="center_login" onClick={goToMain}>로그인</button>
-        </div>
+  const goToMain = () => {
+    navigate('/Main');
+  };
 
-     
-        <div class="content_foot">
-            <a href="#none">비밀번호를 잊으셨나요?</a>
+  return (
+    <div class="content">
+      <div class="content_head">
+        <span>westagram</span>
+      </div>
+
+      <div class="content_center">
+        <div class="center_body">
+          <input
+            className="center_id"
+            type="text"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          />
+          <input className="center_pw" type="password" placeholder="비밀번호" />
         </div>
+        <button className="center_login" onClick={goToMain}>
+          로그인
+        </button>
+      </div>
+
+      <div class="content_foot">
+        <a href="#none">비밀번호를 잊으셨나요?</a>
+      </div>
     </div>
-</>
-      );
+  );
 };
-
 
 export default Login;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,63 +1,60 @@
 import React from 'react';
-import "../Main/Main.css"
-
+import '../Main/Main.scss';
 
 const Main = () => {
-    return(
-<>
-    <nav class="nav_head">
+  return (
+    <>
+      <nav class="nav_head">
         <div class="head_camera">
-            <img src="camera.png" width="10%" />
-            <span>westagram</span>
+          <img src="camera.png" width="10%" />
+          <span>westagram</span>
         </div>
         <button class="head_search">검색</button>
         <div>
-            <img src="explore.png" width="100%" class="head_explore" />
+          <img src="explore.png" width="100%" class="head_explore" />
         </div>
         <div>
-            <img src="heart.png" width="100%" class="head_heart" />
+          <img src="heart.png" width="100%" class="head_heart" />
         </div>
-    </nav>
-    <main>
+      </nav>
+      <main>
         <div class="div.feeds">
-            <article>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-
-            </article>
+          <article>
+            <div />
+            <div />
+            <div />
+            <div />
+          </article>
         </div>
         <div class="div.main_right">
-            <div class="main_right_id">
-            </div>
-            <div class="main_right_story">
-                <ul>
-                    <span>스토리</span>
-                    <button>모두보기</button>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                    <li>4</li>
-                </ul>
-            </div>
-            <div class="main_right_recommand">
-                <ul>
-                    <span>회원님을 위한 추천</span>
-                    <button>모두보기</button>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                    <li>4</li>
-                </ul>
-            </div>
-            <div class="main_right_hongbo">
-                <span>Instagram 정보/지원/홍보 센터/</span>
-            </div>
+          <div class="main_right_id" />
+          <div class="main_right_story">
+            <ul>
+              <span>스토리</span>
+              <button>모두보기</button>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+            </ul>
+          </div>
+          <div class="main_right_recommand">
+            <ul>
+              <span>회원님을 위한 추천</span>
+              <button>모두보기</button>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+            </ul>
+          </div>
+          <div class="main_right_hongbo">
+            <span>Instagram 정보/지원/홍보 센터/</span>
+          </div>
         </div>
-    </main>
-</>
-    );
+      </main>
+    </>
+  );
 };
 
 export default Main;
